2bf76b38d57677a87aac587d6fa6782f8170a45d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2019-12-15 17:10:10 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -80,15 +80,15 @@
     sudo chmod a+x /usr/share/logisim/logisim.jar;
 
     #Instalando o DnsCrypy
-    echo "(13/14)Instalando o DnsCrypt";
-    sudo cp ./NetworkManager.conf /etc/NetworkManager/NetworkManager.conf;
-    sudo cp /etc/dnscrypt-proxy/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml.original  # non-idempotent
-    sudo cp ./dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-    sudo cp ./resolv.conf.override /etc/resolv.conf.override
-    sudo cp ./20-resolv-conf-override /etc/NetworkManager/dispatcher.d/20-resolv-conf-override
-    sudo ln -f /etc/NetworkManager/dispatcher.d/20-resolv-conf-override /etc/NetworkManager/dispatcher.d/pre-up.d/
-    sudo systemctl restart NetworkManager
-    sudo systemctl restart dnscrypt-proxy
+    #echo "(13/14)Instalando o DnsCrypt";
+    #sudo cp ./NetworkManager.conf /etc/NetworkManager/NetworkManager.conf;
+    #sudo cp /etc/dnscrypt-proxy/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml.original  # non-idempotent
+    #sudo cp ./dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+    #sudo cp ./resolv.conf.override /etc/resolv.conf.override
+    #sudo cp ./20-resolv-conf-override /etc/NetworkManager/dispatcher.d/20-resolv-conf-override
+    #sudo ln -f /etc/NetworkManager/dispatcher.d/20-resolv-conf-override /etc/NetworkManager/dispatcher.d/pre-up.d/
+    #sudo systemctl restart NetworkManager
+    #sudo systemctl restart dnscrypt-proxy
 
 
     #Removendo programas inuteis
