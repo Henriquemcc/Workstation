e82eb7ac62c9485282e66e0bacce341d73e619b2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-07-14 15:08:33 -0300

    Removendo compilador de pascal e adicionando os pacotes de idioma português e inglês do libreoffice

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Para o Ubuntu 20
 
-#[Inicio do Programa]
     #Verificando se eh root
     usuario=$(whoami);
     if [ "$usuario" != "root" ]
@@ -62,7 +61,7 @@
 
     #Baixando e Instalando os pacotes apt
     echo "(07/16)Instalando os pacotes pelo grenciador de pacotes APT";
-    sudo apt install virtualbox-6.1 virtualbox-ext-pack google-chrome-stable cinnamon-desktop-environment flatpak gnome-software-plugin-flatpak make ubuntu-make libreoffice evince alien codeblocks audacity gpp gcc g++ fp-compiler brasero soundconverter asunder blueman steam ubuntu-report exfat-utils file-roller gnome-disk-utility usb-creator-gtk transmission-gtk samba ttf-mscorefonts-installer lib32z1 lib32stdc++6 linux-headers-$(uname -r) dkms git gradle gufw ca-certificates-java chrome-gnome-shell libc6:i386 libncurses5:i386 libstdc++6:i386 libbz2-1.0:i386 browser-plugin-freshplayer-pepperflash unrar unrar-free p7zip-full ffmpeg libuchardet0 mpv phantomjs python3-pyxattr rtmpdump bluetooth tlp openjdk-8-jdk openjdk-8-jre libsdl-ttf2.0-0 vlc virtualbox-guest-additions-iso logisim libcanberra-gtk-module libcanberra-gtk3-module iverilog arduino bless openvpn network-manager-openvpn-gnome resolvconf gpa ./Minecraft.deb ./veracrypt-1.24-Update4-Ubuntu-20.04-amd64.deb signal-desktop kdenlive obs-studio mysql-server mysql-client google-earth-pro-stable git-gui -y;
+    sudo apt install virtualbox virtualbox-ext-pack google-chrome-stable cinnamon-desktop-environment flatpak gnome-software-plugin-flatpak make ubuntu-make libreoffice evince alien codeblocks audacity gpp gcc g++ brasero soundconverter asunder blueman steam ubuntu-report exfat-utils file-roller gnome-disk-utility usb-creator-gtk transmission-gtk samba ttf-mscorefonts-installer lib32z1 lib32stdc++6 linux-headers-$(uname -r) dkms git gradle gufw ca-certificates-java chrome-gnome-shell libc6:i386 libncurses5:i386 libstdc++6:i386 libbz2-1.0:i386 browser-plugin-freshplayer-pepperflash unrar unrar-free p7zip-full ffmpeg libuchardet0 mpv phantomjs python3-pyxattr rtmpdump bluetooth tlp openjdk-8-jdk openjdk-8-jre libsdl-ttf2.0-0 vlc virtualbox-guest-additions-iso logisim libcanberra-gtk-module libcanberra-gtk3-module iverilog arduino bless openvpn network-manager-openvpn-gnome resolvconf gpa ./Minecraft.deb ./veracrypt-1.24-Update4-Ubuntu-20.04-amd64.deb signal-desktop kdenlive obs-studio mysql-server mysql-client google-earth-pro-stable git-gui libreoffice-lightproof-pt-br libreoffice-lightproof-en libreoffice-l10n-pt-br libreoffice-l10n-en-gb -y;
 
     #Instalando o flathub
     echo "(08/16)Instalando o repositorio flathub";
@@ -117,5 +116,3 @@
     #Saindo do terminal
     echo "Instalacao concluida!";
     exit;
-
-#[/Fim do Programa]
