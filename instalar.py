a9fd44c65cadbaa1ddf2a5aa3ba0cefd06e1f176
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-11-21 18:59:46 -0300

    Removendo programas não utilizados

--- instalar.py
+++ instalar.py
@@ -4,7 +4,6 @@ import os
 from os.path import expanduser
 import re
 
-
 def executar_comandos_shell(comandos_shell: []):
     """
     Esta funcao serve para executar uma sequencia de comandos shell.
@@ -20,6 +19,7 @@ def executar_comando_shell_sem_erro(comando: str, numero_maximo_de_repeticoes: i
     :param comando: Comando shell que sera executado.
     :param numero_maximo_de_repeticoes: Numero maximo de repeticoes do comando antes que o programa aborte.
     """
+    print(comando)
     repetir = True
     while repetir and numero_maximo_de_repeticoes >= 0:
         try:
@@ -91,23 +91,29 @@ def adicionar_repositorios():
     executar_comandos_shell(comandos)
 
 
-def baixar_arquivos_deb():
+def instalar_programas_deb():
     """
-    Esta funcao serve para baixar os arquivos .deb.
+    Esta funcao serve para baixar e instalar os arquivos .deb.
     """
+
     comandos = \
         [
-            # Baixando o Minecraft
-            "wget https://launcher.mojang.com/download/Minecraft.deb;",
+            # Baixando
+
+            # Minecraft: Não será baixado e nem instalado
+            #"wget https://launcher.mojang.com/download/Minecraft.deb;",
 
             # Veracrypt
             "wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb;",
 
             # .NET Core SDK
             "wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb;",
-            "sudo dpkg -i packages-microsoft-prod.deb;"
+
+            "sudo apt install ./packages-microsoft-prod.deb ./veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb -y;"
         ]
     executar_comandos_shell(comandos)
+
+
 
 
 def instalar_drivers():
@@ -125,11 +131,100 @@ def instalar_programas_apt():
     """
     Esta funcao serve para instalar os programas por meio do apt.s
     """
-    comandos = \
-        [
-            "sudo apt install python virtualbox virtualbox-ext-pack google-chrome-stable cinnamon-desktop-environment flatpak gnome-software-plugin-flatpak make ubuntu-make libreoffice evince alien codeblocks audacity gpp gcc g++ brasero soundconverter asunder blueman steam ubuntu-report exfat-utils file-roller gnome-disk-utility usb-creator-gtk transmission-gtk samba ttf-mscorefonts-installer lib32z1 lib32stdc++6 linux-headers-$(uname -r) dkms git gradle gufw ca-certificates-java chrome-gnome-shell libc6:i386 libncurses5:i386 libstdc++6:i386 libbz2-1.0:i386 browser-plugin-freshplayer-pepperflash unrar unrar-free p7zip-full ffmpeg libuchardet0 mpv phantomjs python3-pyxattr rtmpdump bluetooth tlp openjdk-8-jdk openjdk-8-jre libsdl-ttf2.0-0 vlc virtualbox-guest-additions-iso logisim libcanberra-gtk-module libcanberra-gtk3-module iverilog arduino bless openvpn network-manager-openvpn-gnome resolvconf gpa ./Minecraft.deb ./veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb signal-desktop kdenlive obs-studio mysql-server mysql-client google-earth-pro-stable git-gui libreoffice-lightproof-pt-br libreoffice-lightproof-en libreoffice-l10n-pt-br libreoffice-l10n-en-gb dialog python3-pip python3-setuptools -y;"
-        ]
-    executar_comandos_shell(comandos)
+
+    programas_apt = [
+
+        # Ferramentas do sistema
+        "gufw",  # Firewall
+
+        # Ferramentas de desenvolvimento
+        "python3",
+        "python3-pip",
+        "python3-setuptools",
+        "make",
+        "gcc",
+        "g++",
+        "git",
+        "gcc-multilib",
+        "manpages-dev",
+        "autoconf",
+        "automake",
+        "libtool",
+        "flex",
+        "bison",
+        "gdb",
+        "gcc-doc",
+        "gradle",
+        "git-gui",
+        "openjdk-11-jdk",
+        "ca-certificates-java",
+
+        # VirtualBox
+        "virtualbox",
+        #"virtualbox-ext-pack",
+        "virtualbox-guest-additions-iso",
+
+        # Google Chrome
+        "google-chrome-stable",
+        "chrome-gnome-shell",
+
+        # LibreOffice
+        "libreoffice",
+        "libreoffice-lightproof-pt-br",
+        "libreoffice-lightproof-en",
+        "libreoffice-l10n-pt-br",
+        "libreoffice-l10n-en-gb",
+
+        # Flatpak
+        "flatpak",
+        "gnome-software-plugin-flatpak",
+
+        # Outros programas
+        "evince",
+        "alien",
+        "audacity",
+        "brasero",
+        "asunder",
+        "file-roller",
+        "gnome-disk-utility",
+        "usb-creator-gtk",
+        "transmission-gtk",
+        "steam",
+        "signal-desktop",
+        "kdenlive",
+        "obs-studio",
+        "gpa",
+        "logisim",
+
+        # VPN
+        "openvpn",
+        "network-manager-openvpn-gnome",
+        "resolvconf",
+
+        # Utilitários do sistema
+        "blueman",
+        "ubuntu-report",
+        "exfat-utils",
+        "samba",
+        "linux-headers-$(uname -r)",
+        "dkms",
+        "unrar",
+        "unrar-free",
+        "p7zip-full",
+        "ffmpeg",
+        "unattended-upgrades"
+        # Não serão instalados: bless, dialog, libcanberra-gtk-module ,libcanberra-gtk3-module
+    ]
+
+    comando = "sudo apt install "
+
+    for i in programas_apt:
+        comando += i
+        comando += " "
+
+    comando += "-y;"
+
+    executar_comando_shell_sem_erro(comando)
 
 
 def instalar_flathub():
@@ -249,9 +344,39 @@ def desinstalar_programas_inuteis():
     """
     Esta funcao serve para desinstalar programas inuteis.
     """
+
+    programas_inuteis = [
+
+        "aisleriot",
+        "five-or-more",
+        "hitori",
+        "iagno",
+        "gnome-klotski",
+        "lightsoff",
+        "gnome-mahjongg",
+        "gnome-mines",
+        "gnome-nibbles",
+        "quadrapassel",
+        "four-in-a-row",
+        "gnome-robots",
+        "gnome-sudoku",
+        "swell-foop",
+        "tali",
+        "gnome-taquin",
+        "gnome-tetravex",
+        "gnome-chess",
+        "browser-plugin-freshplayer-pepperflash",
+        "flashplugin-installer"
+    ]
+
+    comando_remover_programas_inuteis = "sudo apt autoremove "
+    for i in programas_inuteis:
+        comando_remover_programas_inuteis += i + " "
+    comando_remover_programas_inuteis += "-y;"
+
     comandos = \
         [
-            "sudo apt autoremove aisleriot five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex gnome-chess browser-plugin-freshplayer-pepperflash flashplugin-installer -y;",
+            comando_remover_programas_inuteis,
             "sudo apt autoclean -y;"
         ]
     executar_comandos_shell(comandos)
@@ -341,18 +466,6 @@ def instalar_extensoes_visual_studio_code():
     executar_comandos_shell(comandos)
 
 
-def instalar_fontes_de_texto():
-    """
-    Esta funcao serve para instalar fontes de texto.
-    """
-    comandos = \
-        [
-            "sudo fc-cache;",
-            "sudo fc-match Arial;"
-        ]
-    executar_comandos_shell(comandos)
-
-
 def configurar_alias():
     """
     Esta funcao serve para configurar os alias.
@@ -366,79 +479,96 @@ def configurar_alias():
 
 
 def main():
+
+    parte = 0
+    total = 17
+
     # Alterando o mirror do ubuntu
-    print("(01/18)Alterando o mirror do Ubuntu")
+    parte += 1
+    print("(", parte, "/", total, ")", "Alterando o mirror do Ubuntu")
     alterar_mirror()
 
     # Instalando prérequisitos
-    print("(02/18)Instalando os softwares prerequisitos")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando os softwares prerequisitos")
     instalar_prerequisitos()
 
     # Atualizando o sistema
-    print("(03/18)Atualizando o sistema")
+    parte += 1
+    print("(", parte, "/", total, ")", "Atualizando o sistema")
     atualizar()
 
     # Fazendo download dos repositórios necessário
-    print("(04/18)Adicionando os repositórios")
+    parte += 1
+    print("(", parte, "/", total, ")", "Adicionando os repositórios")
     adicionar_repositorios()
-    baixar_arquivos_deb()
+    instalar_programas_deb()
 
     # Atualizando as listas de repositórios
-    print("(05/18)Atualizando a lista de repositorios")
+    parte += 1
+    print("(", parte, "/", total, ")", "Atualizando a lista de repositorios")
     atualizar()
 
     # Obtendo automaticamente os novos drivers
-    print("(06/18)Instalando os drivers")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando os drivers")
     instalar_drivers()
 
     # Baixando e Instalando os pacotes apt
-    print("(07/18)Instalando os pacotes pelo grenciador de pacotes APT")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando os pacotes pelo grenciador de pacotes APT")
     instalar_programas_apt()
 
     # Instalando o flathub
-    print("(08/18)Instalando o repositorio flathub")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando o repositorio flathub")
     instalar_flathub()
 
     # Baixando e Instalando os pacotes snap
-    print("(09/18)Instalando pacotes snap")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando pacotes snap")
     instalar_programas_snap()
 
     # Baixando e Instalando os pacotes pip
-    print("(10/18)Instalando pacotes pip")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando pacotes pip")
     instalar_programas_pip()
 
     # Instalando o compilador de Rust Lang e Go Lang
-    print("(11/18)Instalando o compilador de Rust Lang e Go Lang")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando o compilador de Rust Lang e Go Lang")
     instalar_go_lang()
     instalar_rust_lang()
 
-    # Instalando fontes
-    print("(12/18)Instalando as fontes")
-    instalar_fontes_de_texto()
-
     # Instalando o logisim
-    print("(13/18)Instalando o Logisim")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando o Logisim")
     instalar_logisim()
 
     # Instalando a ultima versao do Youtube-dl
-    print("(14/18)Instalando a ultima versao do Youtube-dl")
+    parte += 1
+    print("(", parte, "/", total, ")", "Instalando a ultima versao do Youtube-dl")
     instalar_youtube_dl()
 
     # Removendo programas inuteis
-    print("(15/18)Desinstalando programas inuteis")
+    parte += 1
+    print("(", parte, "/", total, ")", "Desinstalando programas inuteis")
     desinstalar_programas_inuteis()
 
     # Configurando atualizacoes automaticas
-    print("(16/18)Configurando atualizacoes automaticas")
+    parte += 1
+    print("(", parte, "/", total, ")", "Configurando atualizacoes automaticas")
     configurar_atualizacoes_automaticas()
 
     # Configurando o Visual Studio Code
-    print("(17/18)Configurando o Visual Studio Code")
+    parte += 1
+    print("(", parte, "/", total, ")", "Configurando o Visual Studio Code")
     configurar_visual_studio_code()
     instalar_extensoes_visual_studio_code()
 
     # Criando alias
-    print("(18/18)Criando alias")
+    parte += 1
+    print("(", parte, "/", total, ")", "Criando alias")
     configurar_alias()
 
     # Saindo do terminal
